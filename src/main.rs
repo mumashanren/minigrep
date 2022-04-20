979ba63ee46335f598111265c2eddf137c7ca3b1
牧码山人 <mumashanren@gmail.com>
2022-04-20 20:04:18 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for '{}' In file '{}'", config.query, config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
